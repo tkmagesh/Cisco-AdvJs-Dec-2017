df4dbd70ae8c2f2aa7abc688974d6fea0d161e52
Magesh <tkmagesh77@gmail.com>
2017-12-14 14:17:05 +0530

    calculator solution

--- 01-Language/src/calculator.js
+++ 01-Language/src/calculator.js
@@ -1,3 +1,21 @@
 function add(x,y){
-	return x + y;	
+	function parseArg(n){
+		if (Array.isArray(n)) return add.apply(undefined, n);
+		if (typeof n === 'function') return parseArg(n());
+		return isNaN(n) ? 0 : parseInt(n,10);	
+	}
+	return arguments.length <= 1 ? parseArg(arguments[0]) : parseArg(arguments[0]) + add([].slice.call(arguments, 1));
 }
+
+/*
+this -> invocation context
+
+When a function is
+	- invoked as a method of an object (obj)
+		this -> obj
+	- invoked as a function
+		this -> global scope (window in the browser)
+	- invoked using the 'call' method of the function
+
+	- invoked using the 'apply' method of the function
+*/
