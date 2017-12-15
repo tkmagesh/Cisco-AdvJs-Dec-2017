63fb94f27cd09309b2ab69866731a9ee32baf759
Magesh <tkmagesh77@gmail.com>
2017-12-15 10:37:36 +0530

    sort implementation

--- 03-FP/products.js
+++ 03-FP/products.js
@@ -24,23 +24,60 @@ groupBy
 describe('Sort', function(){
 	describe('Default sort [products by id]', function(){
 		function sort(){
-			/*  ...  */
+			for(var i=0; i < products.length-1; i++)
+				for(var j=i+1; j < products.length; j++)
+					if (products[i].id > products[j].id){
+						var temp = products[i];
+						products[i] = products[j];
+						products[j] = temp;
+					}
 		}
+		sort();
 		console.table(products);
 	})
 	describe('Sort any list by any attribute', function(){
-		function sort(){
-			
+		function sort(list, attrName){
+			for(var i=0; i < list.length-1; i++)
+				for(var j=i+1; j < list.length; j++)
+					if (list[i][attrName] > list[j][attrName]){
+						var temp = list[i];
+						list[i] = list[j];
+						list[j] = temp;
+					}
 		}
 		describe('Products by name', function(){
-			//sort();
+			sort(products, 'name');
 			console.table(products);
 		});
 
 		describe('Products by cost', function(){
-			//sort();
+			sort(products, 'cost');
 			console.table(products);
 		});
+	});
+	describe('Sort any list by any comparer', function(){
+		function sort(list, comparer){
+			for(var i=0; i < list.length-1; i++)
+				for(var j=i+1; j < list.length; j++){
+					var compareResult = comparer(list[i], list[j])
+					if (compareResult > 0){
+						var temp = list[i];
+						list[i] = list[j];
+						list[j] = temp;
+					}
+				}
+		}
+		describe('Products by value [ cost * units ]', function(){
+			var productComparerByValue = function(p1, p2){
+				var p1Value = p1.cost * p1.units,
+					p2Value = p2.cost * p2.units;
+				if (p1Value < p2Value) return -1;
+				if (p1Value > p2Value) return 1;
+				return 0;
+			}
+			sort(products, productComparerByValue);
+			console.table(products);
+		})
 	});
 });
 
