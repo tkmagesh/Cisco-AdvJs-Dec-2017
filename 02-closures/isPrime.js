f4e8627f70763f4e1056b3191515ed416db4b67b
Magesh <tkmagesh77@gmail.com>
2017-12-15 09:32:21 +0530

    memoize implementation - part 1

--- 02-closures/isPrime.js
+++ 02-closures/isPrime.js
@@ -15,7 +15,43 @@ var isPrime = (function(){
 				cache[n] = false;
 				break;
 			}
-		return cache[n]
+		return cache[n];
 	}
 	return isPrime;
 })();
+
+var isOddOrEven = (function(){
+	var cache = {};
+	function isOddOrEven(n){
+		if (typeof cache[n] !== 'undefined')
+			return cache[n];
+		
+		console.log('processing ', n);
+		cache[n] = n % 2 === 0 ? 'even' : 'odd';
+		return cache[n];
+	}
+	return isOddOrEven;
+})();
+
+
+function memoize(fn){
+	var cache = {};
+	return function(n){
+		if (typeof cache[n] !== 'undefined')
+			return cache[n];
+		cache[n] = fn(n);
+		return cache[n];
+	}
+}
+
+var isOddOrEven = memoize(function(n){
+	console.log('processing ', n);
+	return n % 2 === 0 ? 'even' : 'odd';
+});
+
+var isPrime = memoize(function(n){
+	console.log('processing ', n);
+	for(var index = 2; index <= (n/2); index++)
+		if(n % index === 0) return false;
+	return true;
+})
